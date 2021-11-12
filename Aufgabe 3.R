5ffc1997afca40f6e533a1fbd33c7857ff8006ae
ceotimritter <78644108+ceotimritter@users.noreply.github.com>
2021-11-12 10:37:40 +0100

    Update Aufgabe 3.R

--- Aufgabe 3.R
+++ Aufgabe 3.R
@@ -31,3 +31,29 @@ parentsedu[parents <= "some college"] = "no college degree"
 parentsedu
 
 chisq.test(parentsedu, test.preparation.course)
+
+
+
+prepN   <- sum(prep$parental.level.of.education == "some high school")
+noprepN <- sum(noprep$parental.level.of.education == "some high school")
+sumN <- sum(edu$parental.level.of.education == "some high school")
+
+prepHs <- sum(prep$parental.level.of.education == "high school") + sum(prep$parental.level.of.education == "some college") 
+noprepHs <- sum(noprep$parental.level.of.education == "high school") + sum(noprep$parental.level.of.education == "some college")
+sumHs <- sum(edu$parental.level.of.education == "high school") + sum(edu$parental.level.of.education == "some college")
+
+prepC <- sum(prep$parental.level.of.education == "associate's degree") + sum(prep$parental.level.of.education == "bachelor's degree") + sum(prep$parental.level.of.education == "master's degree")
+noprepC <- sum(noprep$parental.level.of.education == "associate's degree") + sum(noprep$parental.level.of.education == "bachelor's degree") + sum(noprep$parental.level.of.education == "master's degree")
+sumC <- sum(edu$parental.level.of.education == "associate's degree") + sum(edu$parental.level.of.education == "bachelor's degree") + sum(edu$parental.level.of.education == "master's degree")
+
+matrix(c(prepN,noprepN,sumN,prepHs,noprepHs,sumHs,prepC,noprepC,sumC), ncol=3, byrow = T)
+
+test <- matrix(c(prepN,noprepN,prepHs,noprepHs,prepC,noprepC), ncol=2, byrow = T)
+
+row.names(test) <- c("kein Abschluss","High School Abschluss","College Abschluss")
+colnames(test) <- c("Teilnahme am Vorkurs","Keine Teilnahme am Vorkurs")
+
+mosaicplot(test, col=c("black","white"))
+
+chisq.test(as.numeric(factor(edu$test.preparation.course)), as.numeric(factor(edu$parental.level.of.education)))
+chisq.test(test)
